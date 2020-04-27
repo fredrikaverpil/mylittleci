dba6c3cee00185bc52a9a38d1a1f93b5676eaff5
Fredrik Averpil <fredrik@averpil.com>
2020-04-27 14:53:27 +0200

    Add loguru logger

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,23 @@
 """Pytest configuration."""
 
 import pathlib
+from typing import Any
 
 import pytest
+
+from mylittleci.lib.logger import default_logger
 
 
 @pytest.fixture(name="repo_root")
 def fixture_repo_root() -> pathlib.Path:
     """Return the repository root."""
     return pathlib.Path("..").resolve()
+
+
+@pytest.fixture(name="logger")
+def fixture_logger() -> Any:
+    """Return the loguru logger.
+
+    See ``mylittleci.lib.logger`` for more info.
+    """
+    return default_logger()
